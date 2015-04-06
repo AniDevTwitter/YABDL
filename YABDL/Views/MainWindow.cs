06603b8e4b476268a97f16182a235a33bd2210c6
Uwy <uwy@live.fr>
2015-04-06 22:41:06 +0200

    Gtk binding half broken

--- YABDL/Views/MainWindow.cs
+++ YABDL/Views/MainWindow.cs
@@ -3,14 +3,13 @@ using Gtk;
 
 namespace YABDL.Views
 {
-    public sealed partial class MainWindow : Gtk.Window
+    public partial class MainWindow : Gtk.Window
     {
         public MainWindow()
             : base(Gtk.WindowType.Toplevel)
         {
             this.Build();
         }
-
 
         protected override bool OnDeleteEvent(Gdk.Event evnt)
         {
